89e93d9a0f3550b91c534cdb94ea7ce080ee3f14
Nathan Museve <musevenathan@gmail.com>
2024-08-14 18:50:25 +0300

    styled the 404 error page.

--- Bookstore/settings.py
+++ Bookstore/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2ga_sak$u+xc3)7(b#tt3-y@3mc&(*hq@!@-flfd9dy25#&*&o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
